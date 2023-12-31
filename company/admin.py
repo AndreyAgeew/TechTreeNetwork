1e6afd0229f2dc90984ee82ef929be39b8634663
Andrey Ageew <faceofdeviler@gmail.com>
2023-12-31 22:45:24 +0300

    Implemented admin panels for the Company

--- company/admin.py
+++ company/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Company, Product
 
-# Register your models here.
+
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'country', 'city', 'supplier', 'debt', 'created_at')
+    list_filter = ('city',)
+    search_fields = ('name', 'city')
+    actions = ['clear_debt']
+
+    def clear_debt(self, request, queryset):
+        queryset.update(debt=0)
+
+    clear_debt.short_description = 'Очистить задолженность'
+
+    class Meta:
+        model = Company
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'model', 'release_date', 'company']
+    list_filter = ['company']
+    search_fields = ['name', 'model']
