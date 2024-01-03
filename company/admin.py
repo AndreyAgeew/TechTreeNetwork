40137e1d265c705660085dd9c692ed4ba0c797e6
Andrey Ageew <faceofdeviler@gmail.com>
2024-01-03 16:14:41 +0300

    The view_supplier_link method creates a link to the supplier edit page.

--- company/admin.py
+++ company/admin.py
@@ -1,25 +1,33 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
+
 from .models import Company, Product
 
 
 @admin.register(Company)
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'country', 'city', 'supplier', 'debt', 'created_at')
+    list_display = ('name', 'email', 'country', 'city', 'view_supplier_link', 'debt', 'created_at')
     list_filter = ('city',)
     search_fields = ('name', 'city')
     actions = ['clear_debt']
+
+    def view_supplier_link(self, obj):
+        if obj.supplier:
+            url = reverse("admin:company_company_change", args=[obj.supplier.pk])
+            return format_html('<a href="{}">{}</a>', url, obj.supplier.name)
+        return '-'
+
+    view_supplier_link.short_description = 'Поставщик'
 
     def clear_debt(self, request, queryset):
         queryset.update(debt=0)
 
     clear_debt.short_description = 'Очистить задолженность'
 
-    class Meta:
-        model = Company
-
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'model', 'release_date', 'company']
-    list_filter = ['company']
-    search_fields = ['name', 'model']
+    list_display = ('name', 'model', 'release_date', 'company')
+    list_filter = ('company',)
+    search_fields = ('name', 'model')
