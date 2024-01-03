9bfc295005bdb99bf05accc5be0a9ab631e59a9f
Andrey Ageew <faceofdeviler@gmail.com>
2024-01-03 16:21:36 +0300

    ViewSet: CompanyViewSet is configured to handle CRUD requests for Company objects.

--- company/views.py
+++ company/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Company
+from .serializers import CompanySerializer
 
-# Create your views here.
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['country']
+
+    def perform_update(self, serializer):
+        serializer.save(debt=self.get_object().debt)  # Запретить обновление debt
